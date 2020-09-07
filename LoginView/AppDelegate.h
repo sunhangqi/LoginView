98ca2c9b3f9e6a9e96f8e01b3558a6aee6fa0962
SunHangqi <sunhangqi@gmail.com>
2020-09-08 06:53:26 +0800

    Initial Commit

---
+++ LoginView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LoginView
+//
+//  Created by one on 2020/9/8.
+//  Copyright © 2020 one. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
